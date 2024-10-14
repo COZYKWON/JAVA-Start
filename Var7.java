ac85a88146a5659362ac43f9fcdc5a1318529675
COZYKWON <cozykwon03@naver.com>
2024-10-15 00:44:43 +0900

    Add files via upload

---
+++ Var7.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var7 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 10.5;
+        boolean c = true;
+        char d = 'A';
+        String e = "Hello my name is Jongwon";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
---
+++ VarEx3.java
@@ -0,0 +1,14 @@
+package variable.ex;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long num1;
+        num1 = 10000000000L;
+        System.out.println(num1);
+        System.out.println("num1 = " + num1);
+
+        boolean tf = true;
+        System.out.println(tf);
+    }
+}
