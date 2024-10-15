eef9f3e1025d80eee1e07d1c4ff48e062fab259a
COZYKWON <cozykwon03@naver.com>
2024-10-15 23:50:45 +0900

    Add files via upload

---
+++ CondOp_1m.java
@@ -0,0 +1,13 @@
+package cond.ex;
+
+public class CondOp_1m {
+
+    public static void main(String[] args) {
+
+        int a = 30;
+        int b = 20;
+
+        int result = (a > b) ? a : b;
+        System.out.println("더 큰 숫자는 " + result + "입니다.");
+    }
+}
